86ab952a53a38b9015f47f1d6b9764d9a126b27c
labyrinthofdreams <noemail@localhost.localhost>
2012-12-14 11:12:04 +0200

    Added exception handling to main()

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <QtGui/QApplication>
+#include <QMessageBox>
+#include <QString>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    
-    return a.exec();
+
+    try
+    {
+        MainWindow w;
+        w.show();
+
+        return a.exec();
+    }
+    catch(std::exception& ex)
+    {
+        QMessageBox::critical(0, "Critical error",
+                              QString(ex.what()));
+        a.exit(1);
+    }
+
+    return 1;
 }
