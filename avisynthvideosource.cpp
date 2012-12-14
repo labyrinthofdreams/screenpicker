0588f7ce45df4d68e7f1508bbac635263095eb10
labyrinthofdreams <noemail@localhost.localhost>
2012-12-14 09:01:04 +0200

    Changes

--- avisynthvideosource.cpp
+++ avisynthvideosource.cpp
@@ -32,49 +32,42 @@ AvisynthVideoSource::~AvisynthVideoSource()
 
 void AvisynthVideoSource::load(QString fileName)
 {
-    AVS_Value res = avs_void;
-    try
+    // If you don't store the converted data temporarily,
+    // you will have a memory leak
+    QByteArray tmp = fileName.toLocal8Bit();
+    AVS_Value arg = avs_new_value_string(tmp.constData());
+    AVS_Value res = avsHandle.func.avs_invoke(avsHandle.env, "Import", arg, NULL);
+    if(avs_is_error(res))
     {
-        // If you don't store the converted data temporarily,
-        // you will have a memory leak
-        QByteArray tmp = fileName.toLocal8Bit();
-        AVS_Value arg = avs_new_value_string(tmp.constData());
-        res = avsHandle.func.avs_invoke(avsHandle.env, "Import", arg, NULL);
-        if(avs_is_error(res))
-        {
-            // Throw if our imported script fails
-            throw std::runtime_error(avs_as_string(res));
-        }
-        if(!avs_is_clip(res))
-        {
-            throw std::runtime_error("Imported script did not return a video clip");
-        }
-
-        avsHandle.clip = avsHandle.func.avs_take_clip(res, avsHandle.env);
-        info = avsHandle.func.avs_get_video_info(avsHandle.clip);
-        if(!avs_has_video(info))
-        {
-            throw std::runtime_error("Imported script does not have video data");
-        }
-
-        // Convert input to RGB24
-        const char* arg_name[2] = {NULL, "interlaced"};
-        AVS_Value arg_arr[2] = {res, avs_new_value_bool(0)};
-        AVS_Value conv_tmp = avsHandle.func.avs_invoke(avsHandle.env, "ConvertToRGB24",
-                                                       avs_new_value_array(arg_arr, 2), arg_name);
-        if(avs_is_error(conv_tmp))
-        {
-            throw std::runtime_error("Failed to convert input clip to RGB24");
-        }
-
-        res = internal_avs_update_clip(&avsHandle, &info, conv_tmp, res);
-        avsHandle.func.avs_release_value(res);
+        // Throw if our imported script fails
+        throw std::runtime_error(avs_as_string(res));
     }
-    catch(std::exception& ex)
+    if(!avs_is_clip(res))
+    {
+        throw std::runtime_error("Imported script did not return a video clip");
+    }
+
+    avsHandle.clip = avsHandle.func.avs_take_clip(res, avsHandle.env);
+    info = avsHandle.func.avs_get_video_info(avsHandle.clip);
+    if(!avs_has_video(info))
     {
         avsHandle.func.avs_release_value(res);
-        throw;
+        throw std::runtime_error("Imported script does not have video data");
     }
+
+    // Convert input to RGB32
+    const char* arg_name[2] = {NULL, "interlaced"};
+    AVS_Value arg_arr[2] = {res, avs_new_value_bool(0)};
+    AVS_Value conv_tmp = avsHandle.func.avs_invoke(avsHandle.env, "ConvertToRGB",
+                                                   avs_new_value_array(arg_arr, 2), arg_name);
+    if(avs_is_error(conv_tmp))
+    {
+        avsHandle.func.avs_release_value(res);
+        throw std::runtime_error("Failed to convert input clip to RGB24");
+    }
+
+    res = internal_avs_update_clip(&avsHandle, &info, conv_tmp, res);
+    avsHandle.func.avs_release_value(res);
 }
 
 QImage AvisynthVideoSource::getFrame(unsigned frameNumber)
@@ -93,16 +86,16 @@ QImage AvisynthVideoSource::getFrame(unsigned frameNumber)
 
     qDebug() << frame->pitch << frame->row_size;
 
-    avsHandle.func.avs_release_video_frame(frame);
-
-    QImage image(info->width, info->height, QImage::Format_RGB888);
-    const unsigned scanlineLength = info->width * 3;
-    const int pitch = avs_get_pitch_p(frame, 0);
-    const BYTE* data = avs_get_read_ptr_p(frame, 0);
+    QImage image(info->width, info->height, QImage::Format_ARGB32);
+    const unsigned scanlineLength = info->width * 4;
+    const int pitch = avs_get_pitch(frame);
+    const BYTE* data = avs_get_read_ptr(frame);
     for(int y = 0; y < info->height; ++y)
     {
         memcpy(image.scanLine(y), data + y * pitch, scanlineLength);
     }
+
+    avsHandle.func.avs_release_video_frame(frame);
 
     return image;
 }
