007ca1c8b0fe309b76caebe4b945dc02bde3c07c
labyrinthofdreams <noemail@localhost.localhost>
2013-06-11 03:50:28 +0300

    Add member functions to generator class declaration

--- videoframegenerator.h
+++ videoframegenerator.h
@@ -2,10 +2,14 @@
 #define VFG_VIDEOFRAMEGENERATOR_H
 
 #include <QObject>
+#include <QList>
 
+// Forward declarations
 namespace vfg {
     class VideoFrameGrabber;
 }
+
+class QImage;
 
 namespace vfg {
 
@@ -14,13 +18,22 @@ class VideoFrameGenerator : public QObject
     Q_OBJECT
 public:
     explicit VideoFrameGenerator(vfg::VideoFrameGrabber *frameGrabber, QObject *parent = 0);
+
+    void start();
+    void pause();
+    void resume();
+    void reset();
+    bool isRunning() const;
+    bool enqueue(unsigned frame);
     
 signals:
+    void frameReady(QImage frame);
     
 public slots:
     
 private:
     vfg::VideoFrameGrabber *frameGrabber;
+    QList<unsigned> frames;
 };
 
 } // namespace vfg
