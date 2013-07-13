065033ce322016d4083896f6f71106cb7df4ada7
labyrinthofdreams <noemail@localhost.localhost>
2013-07-14 01:19:24 +0300

    ?

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -778,7 +778,7 @@ void MainWindow::on_actionOptions_triggered()
         QSettings cfg("config.ini", QSettings::IniFormat);
 
         ui->unsavedWidget->setMaxThumbnails(cfg.value("maxthumbnails").toInt());
-        dvdProcessor->setProcessor(cfg.value("dgindexexecpath").toString());
+        dvdProcessor->setProcessor(cfg.value("dgindexexecpath").toString());       
     }
 }
 
