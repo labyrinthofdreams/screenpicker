103d80269186f96472ba159d19eebeb6016ff1ef
labyrinthofdreams <noemail@localhost.localhost>
2013-07-15 23:34:07 +0300

    Add custom exception for video sources

--- abstractvideosource.h
+++ abstractvideosource.h
@@ -1,11 +1,20 @@
 #ifndef ABSTRACTVIDEOSOURCE_H
 #define ABSTRACTVIDEOSOURCE_H
 
+#include <stdexcept>
+#include <string>
 #include <QString>
 #include <QImage>
 
 namespace vfg
 {
+    class VideoSourceException : public std::runtime_error
+    {
+    public:
+        VideoSourceException(const char* msg) : std::runtime_error(msg) {}
+        VideoSourceException(const std::string& msg) : std::runtime_error(msg) {}
+    };
+
     class AbstractVideoSource
     {
     public:
