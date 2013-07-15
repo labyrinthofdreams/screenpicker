1347f4422b559c825b1ba3f69336be2ed1f1aa34
labyrinthofdreams <noemail@localhost.localhost>
2013-07-15 23:35:42 +0300

    Replace std::runtime_error with custom exception class

--- scriptparser.h
+++ scriptparser.h
@@ -6,6 +6,13 @@ class QString;
 
 namespace vfg {
 
+class ScriptParserTemplateException : public std::runtime_error
+{
+public:
+    explicit ScriptParserTemplateException(const char* msg) : std::runtime_error(msg) {}
+    explicit ScriptParserTemplateException(const std::string& msg) : std::runtime_error(msg) {}
+};
+
 class ScriptParser
 {
 public:
