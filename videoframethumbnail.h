9e5fc526d1697a98fea4a8831693a2b02f5a9ca3
labyrinthofdreams <noemail@localhost.localhost>
2014-02-10 13:41:42 +0200

    Add doxygen documentation

--- videoframethumbnail.h
+++ videoframethumbnail.h
@@ -14,18 +14,34 @@ namespace vfg
 {
     /**
      * @brief The VideoFrameThumbnail class
-     * Displays video thumbnail, emits a frame number when selected
      */
     class VideoFrameThumbnail : public QWidget
     {
         Q_OBJECT
     public:
-        explicit VideoFrameThumbnail(const unsigned frame, QPixmap thumbnail,
+        /**
+         * @brief VideoFrameThumbnail Constructor
+         * @param frame Frame number
+         * @param thumbnail Thumbnail image
+         * @param parent Owner of the widget
+         */
+        explicit VideoFrameThumbnail(int frame, QPixmap thumbnail,
                                      QWidget *parent = 0);
 
+        /**
+         * @brief markSelected Highlights the thumbnail
+         */
         void markSelected();
+
+        /**
+         * @brief markUnselected Removes highlight from thumbnail
+         */
         void markUnselected();
 
+        /**
+         * @brief frameNum
+         * @return Frame number
+         */
         unsigned frameNum() const;
 
     private:
@@ -35,6 +51,9 @@ namespace vfg
 
         unsigned frameNumber;
 
+        /**
+         * @brief updateFrameSize Scales the thumbnail to the widget size
+         */
         void updateFrameSize();
 
     protected:
@@ -44,11 +63,19 @@ namespace vfg
         void paintEvent(QPaintEvent *event);
 
     signals:
+        /**
+         * @brief selected Emits the selected widget
+         * @param thumbnail
+         */
         void selected(vfg::VideoFrameThumbnail* thumbnail);
         void doubleClicked(unsigned frameNumber);
 
     public slots:
 
+        /**
+         * @brief doubleClicked Emits the frame number of the widget
+         * @param frameNumber
+         */
     };
 }
 #endif // VIDEOFRAMETHUMBNAIL_H
