a0b9fdd49956886ae83fcc642d89edf8212dcdae
labyrinthofdreams <noemail@localhost.localhost>
2014-02-10 17:03:00 +0200

    Replace unsigned with int

--- abstractvideosource.h
+++ abstractvideosource.h
@@ -22,8 +22,8 @@ namespace vfg
         virtual ~AbstractVideoSource() {}
         virtual void load(QString fileName) = 0;
         virtual bool hasVideo() const = 0;
-        virtual unsigned getNumFrames() const = 0;
-        virtual QImage getFrame(unsigned frameNumber) = 0;
+        virtual int getNumFrames() const = 0;
+        virtual QImage getFrame(int frameNumber) = 0;
         virtual QString getSupportedFormats() = 0;
     };
 }
