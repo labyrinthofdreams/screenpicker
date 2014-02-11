64e7dbb8e19040b6ae76fcbb97fdb6ebd2760e29
labyrinthofdreams <noemail@localhost.localhost>
2014-02-11 10:07:50 +0200

    Add Doxygen comments

--- videosettingswidget.h
+++ videosettingswidget.h
@@ -1,14 +1,14 @@
 #ifndef VFG_VIDEOSETTINGSWIDGET_H
 #define VFG_VIDEOSETTINGSWIDGET_H
 
-#include <QWidget>
 #include <QMap>
 #include <QString>
+#include <QWidget>
 
 // Forward declarations
 class QCloseEvent;
-class QShowEvent;
 class QRect;
+class QShowEvent;
 
 namespace Ui {
     class VideoSettingsWidget;
@@ -16,6 +16,9 @@ namespace Ui {
 
 namespace vfg {
 
+/**
+ * @brief Common DVD resolutions
+ */
 enum class Resolution : int {
     OTHER = 0,
     NTSC_169 = 1,
@@ -32,24 +35,61 @@ enum class Deinterlace : int {
 
 namespace ui {
 
+/**
+ * @brief The VideoSettingsWidget class
+ *
+ * The video settings widget contains UI elements
+ * for modifying the input video
+ */
 class VideoSettingsWidget : public QWidget
 {
-    Q_OBJECT
-    
+    Q_OBJECT   
 public:
+    /**
+     * @brief Constructor
+     * @param parent Owner of the widget
+     */
     explicit VideoSettingsWidget(QWidget *parent = 0);
+
+    /**
+     * @brief Destructor
+     **/
     ~VideoSettingsWidget();
 
+    /**
+     * @brief Retrieve current settings
+     * @return Settings as a map
+     */
     QMap<QString, int> getSettings() const;
+
+    /**
+     * @brief Reset all settings to default values
+     */
     void resetSettings();
     
 private slots:
+    /**
+     * @brief Handle DVD resolution dropdown change
+     *
+     * Sets the resolution in the input boxes when a DVD resolution
+     * is selected from the dropdown menu
+     * @param index
+     */
     void on_cboxDvdResolution_activated(int index);
 
+    /**
+     * @brief Apply changes and emit \link settingsChanged() \endlink
+     */
     void on_pushButton_clicked();
 
+    /**
+     * @brief Emit \link cropChanged() \endlink with area to crop
+     */
     void handleCropChange();
 
+    /**
+     * @brief Revert previously applied crop
+     */
     void on_btnRevertCrop_clicked();
 
 private:
@@ -66,16 +106,24 @@ protected:
     void closeEvent(QCloseEvent *event);
 
 signals:
-    void settingsChanged();
     /**
-     * @brief cropChanged emits new crop values
-     * @param area Area to crop on the image
+     * @brief Signal changed settings
+     */
+    void settingsChanged();
+
+    /**
+     * @brief Signal changed crop values
+     * @param area Area to crop
      */
     void cropChanged(QRect area);
 
+    /**
+     * @brief Signal window close event
+     */
     void closed();
 };
 
 } // namespace ui
 } // namespace vfg
+
 #endif // VFG_VIDEOSETTINGSWIDGET_H
