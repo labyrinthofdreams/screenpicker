bab7bfe1b1e216b240a2fae835dcd4ad7f6e9c75
labyrinthofdreams <noemail@localhost.localhost>
2014-02-18 16:55:32 +0200

    Doxygen documentation

--- abstractvideosource.h
+++ abstractvideosource.h
@@ -10,6 +10,11 @@
 namespace vfg {
 namespace exception {
 
+/**
+ * @brief The VideoSourceError class
+ *
+ * General exception relating to video source errors
+ */
 class VideoSourceError : public std::runtime_error
 {
 public:
@@ -21,19 +26,72 @@ public:
 
 namespace core {
 
+/**
+ * @brief The AbstractVideoSource class
+ *
+ * This base class is responsible for loading video sources
+ * and returning frames from the source
+ */
 class AbstractVideoSource : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor
+     */
     AbstractVideoSource() {}
+
+    /**
+     * @brief Destructor
+     */
     virtual ~AbstractVideoSource() {}
+
+    /**
+     * @brief Load given file
+     * @param fileName Path to video
+     */
     virtual void load(QString fileName) = 0;
+
+    /**
+     * @brief Get video status
+     * @return True if video is available, otherwise false
+     */
     virtual bool hasVideo() const = 0;
+
+    /**
+     * @brief Get number of frames in the video
+     * @return Number of frames
+     */
     virtual int getNumFrames() const = 0;
+
+    /**
+     * @brief Get frame from the video source
+     *
+     * This function does not throw
+     *
+     * @param frameNumber Frame to request
+     * @return The requested frame. Empty QImage on error.
+     */
     virtual QImage getFrame(int frameNumber) = 0;
+
+    /**
+     * @brief Get supported files by extension
+     *
+     * Example: "Avisynth files (*.avs,*.avsi)"
+     * Example2: "Video files (*.mkv,*.avi)"
+     *
+     * "*" matches all
+     *
+     * @return Supported file extensions as a string
+     */
     virtual QString getSupportedFormats() = 0;
 
-signals:
+signals:    
+    /**
+     * @brief Signals when the video has been loaded
+     *
+     * This signal is emitted from \link load(QString fileName) load \endlink
+     */
     void videoLoaded();
 };
 
