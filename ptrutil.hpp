5c52c33946e152e176a8c6b3ae21716fb4ac7f79
labyrinthofdreams <noemail@localhost.localhost>
2014-05-16 23:57:08 +0300

    Add observer_ptr wrapper for raw pointers

--- ptrutil.hpp
+++ ptrutil.hpp
@@ -32,6 +32,96 @@ THE SOFTWARE.
 
 namespace util {
 
+/**
+ * @brief The observer_ptr class wraps a raw pointer
+ *
+ * An observer_ptr implies no ownership and makes it
+ * more difficult to accidentally do "delete ptr"
+ * or pass it to a function that takes ownership.
+ */
+template <class T>
+class observer_ptr {
+    T* _ptr {nullptr};
+
+public:
+    observer_ptr() = default;
+    observer_ptr(T* ptr) : _ptr(ptr) {}
+
+    observer_ptr(const observer_ptr& other) = default;
+    observer_ptr(observer_ptr&& other) = default;
+
+    observer_ptr& operator=(const observer_ptr& other) = default;
+    observer_ptr& operator=(observer_ptr&& other) = default;
+
+    observer_ptr& operator=(T* new_ptr) { _ptr = new_ptr; return *this; }
+
+    ~observer_ptr() = default;
+
+    operator bool() const { return _ptr; }
+
+    T& operator *() const { return *_ptr; }
+
+    T* operator ->() const { return _ptr; }
+
+    /**
+     * @brief Get raw pointer to observed object
+     * @return Pointer to observed object
+     */
+    T* get() const { return _ptr; }
+
+    /**
+     * @brief Set internal pointer value to nullptr and return old pointer
+     * @return Pointer to observed object
+     */
+    T* release() { T* tmp = _ptr; _ptr = nullptr; return tmp; }
+
+    /**
+     * @brief Set internal pointer value to nullptr
+     */
+    void reset() { _ptr = nullptr; }
+};
+
+template <typename T1, typename T2>
+inline
+bool operator==(const observer_ptr<T1>& lhs, const observer_ptr<T2>& rhs) {
+    return lhs.get() == rhs.get();
+}
+
+template <typename T1, typename T2>
+inline
+bool operator!=(const observer_ptr<T1>& lhs, const observer_ptr<T2>& rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T1, typename T2>
+inline
+bool operator==(const observer_ptr<T1>& lhs, const T2* rhs) {
+    return lhs.get() == rhs;
+}
+
+template <typename T1, typename T2>
+inline
+bool operator!=(const observer_ptr<T1>& lhs, const T2* rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T1, typename T2>
+inline
+bool operator==(const T1* lhs, const observer_ptr<T2>& rhs) {
+    return lhs == rhs.get();
+}
+
+template <typename T1, typename T2>
+inline
+bool operator!=(const T1* lhs, const observer_ptr<T2>& rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T, typename... Args>
+observer_ptr<T> make_observer(Args&&... args) {
+    return observer_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args)
 {
