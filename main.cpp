bdbad466310c48e976745f9bd35d138694c6d75c
labyrinthofdreams <noemail@localhost.localhost>
2014-08-24 17:42:25 +0300

    Add logger

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <QMessageBox>
 #include <QSettings>
 #include <QString>
+#include <QTextStream>
 #include <stdexcept>
 #include "mainwindow.h"
 #include "init.h"
@@ -22,37 +23,68 @@ bool condCopy(const QString& from, const QString& to) {
     return QFile::copy(from, to);
 }
 
+void myMessageHandler(QtMsgType msgType, const QMessageLogContext& ctx, const QString& msg)
+{
+    Q_UNUSED(ctx);
+
+    QString type;
+    switch (msgType) {
+    case QtDebugMsg:
+        type = "Debug";
+        break;
+    case QtWarningMsg:
+        type = "Warning";
+        break;
+    case QtCriticalMsg:
+        type = "Critical";
+        break;
+    case QtFatalMsg:
+        type = "Fatal";
+        break;
+    }
+
+    QString text = QString("[%1] (%2): %3").arg(ctx.category).arg(type).arg(msg);
+
+    QFile outFile("log.txt");
+    outFile.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Append);
+
+    QTextStream ts(&outFile);
+    ts << text << endl;
+}
+
 int main(int argc, char *argv[])
 {
+    // Create config
+    QSettings config("config.ini", QSettings::IniFormat);
+    vfg::config::merge(config, vfg::config::getDefaultSettings());
+
+    if(config.value("enable_logging", false).toBool()) {
+        qInstallMessageHandler(myMessageHandler);
+    }
+
+    // Write scripts
+    QDir current;
+    current.mkdir("scripts");
+
+    condCopy(":/scripts/imagemagick.ini", "scripts/imagemagick.ini");
+    condCopy(":/scripts/gifsicle.ini", "scripts/gifsicle.ini");
+    condCopy(":/scripts/x264.ini", "scripts/x264.ini");
+    condCopy(":/scripts/d2v_template.avs", "scripts/d2v_template.avs");
+    condCopy(":/scripts/default_template.avs", "scripts/default_template.avs");
+
     const QString appName = "ScreenPicker 3.0 (beta) rev1";
     QApplication a(argc, argv);
     a.setApplicationName(appName);
     a.setApplicationDisplayName(appName);
     a.setApplicationVersion(appName);
 
-    try
-    {
-        // Create config
-        QSettings config("config.ini", QSettings::IniFormat);
-        vfg::config::merge(config, vfg::config::getDefaultSettings());
-
-        // Write scripts        
-        QDir current;
-        current.mkdir("scripts");
-
-        condCopy(":/scripts/imagemagick.ini", "scripts/imagemagick.ini");
-        condCopy(":/scripts/gifsicle.ini", "scripts/gifsicle.ini");
-        condCopy(":/scripts/x264.ini", "scripts/x264.ini");
-        condCopy(":/scripts/d2v_template.avs", "scripts/d2v_template.avs");
-        condCopy(":/scripts/default_template.avs", "scripts/default_template.avs");
-
+    try {
         MainWindow w;
         w.show();
 
         return a.exec();
     }
-    catch(std::exception& ex)
-    {
+    catch(std::exception& ex) {
         QMessageBox::critical(0, a.tr("Critical error"),
                               QString(ex.what()));
         a.exit(1);
